0898640b35b9dbb972f752dd71639cfe7ae9deab
RODRIGO NAOHAN RODRIGUES NEILAND <rodrigo.naohan@gmail.com>
2023-11-30 13:45:36 -0400

    Leo, Caua, Mancadas e Neiland

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Header } from './components/login/Header';
 import { Container1 } from './components/login/container1';
+import { Container2 } from './components/login/container2';
 
 import styles from './App.module.css'
 import './globals.css';
@@ -11,6 +12,7 @@ export default function Home() {
 
       <div className={styles.wrapper}>
         <Container1/>
+        <Container2/>
       </div>
     </div>
   )
